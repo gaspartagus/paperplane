5de24116cf5ae4c8b1329a83627708c7e22d6a82
Gaspard <gaspardbenoit@msn.com>
2014-11-20 18:28:43 +0100

    ..

--- stellar.js
+++ stellar.js
@@ -4,9 +4,9 @@ function gravity(solid1,solid2) {
 	var G = 1,
 			Dx = solid2.x - solid1.x,
 			Dy = solid2.y - solid1.y,
-			D4 = Math.pow(Dx*Dx + Dy*Dy, 2),
-			ax = G * solid1.m * solid2.m * Dx / D4,
-			ay = G * solid1.m * solid2.m * Dy / D4;
+			D3 = Math.pow(Dx*Dx + Dy*Dy, 3/2),
+			ax = G * solid1.m * solid2.m * Dx / D3,
+			ay = G * solid1.m * solid2.m * Dy / D3;
 	return [ax,ay];
 }
 
@@ -16,39 +16,50 @@ function Solid(force,options){
 		y: 0,
 		vx: 0,
 		vy: 0,
-		m: 0,
+		m: 1,
 		t: 0,
-		tpl: "<div>-|-></div>",
+		tpl: "",
+		draw: true,
 	};
-	_.assign(this,_.merge(options,DEFAULTS));
+	_.assign(this,_.merge(DEFAULTS,options));
+	var self = this;
 
 	// DOM section
-	var template = _.template(this.tpl);
+	if(this.draw) {
+		var template = _.template(this.tpl);
+		this.el = document.createElement("div");
+		this.el.setAttribute("id",this.name);
+		this.el.innerHTML = template(this);
+		document.body.appendChild(this.el);
+	}
 
-	this.el = document.createElement("div");
-	this.el.innerHTML = template(this);
-
-	document.body.appendChild(this.el);
+	function DOM(){
+		// self.el.style.transform = "translate("+self.x+"px ,"+self.y+"px)";
+		// console.log(self.x,self.y);
+	}
 
 	// Physics section
 	function interactions(solids) {
-		var solid = this;
-		var dt = performance.now()-solid.t;
+		var solid = self;
+		var dt = performance.now()/1000-solid.t;
+
 		if(dt > 500) return;
 		solids.forEach(function(s){
 			if(s!==solid){
-				var A = this.force(solid,s); // self
+				var A = force(solid,s);
 				solid.vx += dt*A[0];
 				solid.vy += dt*A[1];
 				solid.x += dt*solid.vx;
 				solid.y += dt*solid.vy;
 			}
 		})
-		solid.t = performance.now();
+		solid.t = performance.now()/1000;
 	}
 
 	function elapse(solids) {
 		interactions(solids);
+		DOM();
+		// console.log(solids)
 		requestAnimationFrame(function(){
 			elapse(solids);
 		});
@@ -58,5 +69,4 @@ function Solid(force,options){
 
 };
 
-var plane = new Solid(gravity);
 
